d43d0e29caf36d479618e5d51007cfd565c13ee5
Lucas <153150990+LukasO20@users.noreply.github.com>
2023-12-24 00:24:45 -0300

    Refatoração: Footer / Adicionado: funções JS para o "Menu"
    Melhorias e refatoração de código

--- libs/js/default.js
+++ libs/js/default.js
@@ -1,6 +1,8 @@
 //const dynamic_title = $('body main section .div-section-titles h1')
 
 const section = $('section')
+const menu = $('main article')
+
 $(window).on('scroll', function (){
     const scrollTop = $(this).scrollTop()
     const screenHeight = $(this).height()
@@ -17,25 +19,59 @@ $(window).on('scroll', function (){
                 const divBottom = divTop + divHeight
 
                 if(scrollTop >= divTop && scrollTop < divBottom){
-                    const divDynamic = getDivPosition(index)
-                    section.find('.h2-principal').text(divDynamic)
+                    const itemDynamic = itemCssDynamic(index)
+
+                    section.find('.h1-principal').css('font-size', itemDynamic.textSizeH1)
+                    section.find('.h2-principal').css('font-size', itemDynamic.textSizeH2)
+                
+                    menu.find('.li-links').each(function (){
+                        let item = $(this)
+                        switch (index) {
+                            case 1:
+                                if (item.hasClass('li1')){
+                                    return item.addClass('li-links-dynamic')
+                                }
+                                break
+                            case 2:
+                                if (item.hasClass('li2')){
+                                    return item.addClass('li-links-dynamic')
+                                }
+                                break
+                            case 3:
+                                if (item.hasClass('li3')){
+                                    return item.addClass('li-links-dynamic')
+                                }
+                                break
+                            case 4:
+                                if (item.hasClass('li4')){
+                                    return item.addClass('li-links-dynamic')
+                                }
+                                break
+                            default:
+                                return item.removeClass('li-links-dynamic')       
+                        }
+                    })
                 }
             })  
         }
     }
 })
 
-function getDivPosition(index) {
+function itemCssDynamic(index) {
+
+    const itemStyle = {
+        textSizeH1: '',
+        textSizeH2: '',
+    }
+
     switch (index) {
-        case 1:
-            return 'About me'
-        case 2:
-            return 'My skills'
-        case 3:
-            return 'My works'
-        case 4:
-            return 'Contact'
-        default: 
-            return "I'm a web developer"
+        case 0:
+            itemStyle.textSizeH1 = '2.5em'
+            itemStyle.textSizeH2 = '2.1em'
+            return itemStyle
+        default:
+            itemStyle.textSizeH1 = '0em'
+            itemStyle.textSizeH2 = '0em'
+            return itemStyle
     }
 }
