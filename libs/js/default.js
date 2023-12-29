aa002506246bcfe57a0a6808f966543defa123ab
Lucas <153150990+LukasO20@users.noreply.github.com>
2023-12-28 23:18:20 -0300

    Adicionado: Form para envio de e-mail (em andamento) / ajustes design botão "Button up"
    Pequenas melhorias e adicionado Form para envio de e-mail

--- libs/js/default.js
+++ libs/js/default.js
@@ -1,5 +1,7 @@
 const section = $('section')
 const menu = $('main article')
+const sendEmail = $('section .form-email')
+const contentEmail = $('section .form-email .div-contact-content-email')
 
 $(window).on('scroll', function (){
     const scrollTop = $(this).scrollTop()
@@ -16,42 +18,46 @@ $(window).on('scroll', function (){
                 const divHeight = $(this).outerHeight()
                 const divBottom = divTop + divHeight
 
-                if(scrollTop >= divTop && scrollTop < divBottom){
+                if (scrollTop >= divTop && scrollTop < divBottom){
                     const itemDynamic = itemCssDynamic(index)
                   
                     section.find('.h1-principal').css('font-size', itemDynamic.textSizeH1)
                     section.find('.h2-principal').css('font-size', itemDynamic.textSizeH2)
-                
+
+                    
+
                     menu.find('.li-links').each(function (){
                         let item = $(this)
                         switch (index) {
                             case 1:
+                                section.find('.button-up').css('opacity', '0')
                                 if (item.hasClass('li1')){
                                     return item.addClass('li-links-dynamic')
                                 }
                                 break
                             case 2:
+                                section.find('.button-up').css('opacity', '1')
                                 if (item.hasClass('li2')){
                                     return item.addClass('li-links-dynamic')
                                 }
                                 break
                             case 3:
+                                section.find('.button-up').css('opacity', '1')
                                 if (item.hasClass('li3')){
                                     return item.addClass('li-links-dynamic')
                                 }
                                 break
                             case 4:
+                                section.find('.button-up').css('opacity', '1')
                                 if (item.hasClass('li4')){
                                     return item.addClass('li-links-dynamic')
                                 }
                                 break
                             default:
+                                section.find('.button-up').css('opacity', '0')
                                 return item.removeClass('li-links-dynamic')       
                         }
                     })
-
-                    const hideButtonup = section.find('.button-up').hide()
-                    const showButtonup = section.find('.button-up').show()
                 }
             })  
         }
@@ -121,14 +127,31 @@ section.find('.button-up').on('click', function (){
     const divs = $('section .div-section')
 
     if (divs.length){
-        divs.each(function (index) {
-            if (index === 1) {
-                return $('html, body').animate({
-                    scrollTop: divs.eq(index).offset().top 
-                }, 700)
-            }
-        })
-    }
+        const divScroll = divs.eq(1)
 
-    console.log(divs)
+        $('html, body').animate({
+            scrollTop: divScroll.offset().top
+        }, 700)
+    }
+})
+
+$('section .div-section-content-t4 .a-contact-send-email').on('click', function (e){
+    e.preventDefault()
+    try {
+        let name = contentEmail.find('input[name="name"]').val()
+        let email = contentEmail.find('input[name="email"]').val()
+        let message = contentEmail.find('textarea[name="message"]').val()
+
+        if (name !== '' || email !== '' || message !== '') {
+            //sendEmail.submit()
+            console.log('WOW, TUDO CERTO!!')
+            console.log(name)
+            console.log(email)
+            console.log(message)
+        } else {
+            console.log('OPS, TUDO ERRADO!!')
+        }
+    } catch (e) {
+        console.log(`Erro ao enviar e-mail: ${e}`)
+    }
 })
