b2407c99cc18e77dd47f5e565e6ce7a099cba16b
Lucas <153150990+LukasO20@users.noreply.github.com>
2024-06-27 23:07:08 -0300

    Atualização index.js (Refatoração - parte 01)

--- libs/js/index.js
+++ libs/js/index.js
@@ -1,145 +1,126 @@
 // -- GLOBAL generic VARs --
 // Global var is namevar_g
 
-var screen_user_g = $(window)
+var screenUserG = $(window)
 
 // -- MENU FUNCTIONS --
 const menu = $('.article-menu')
-const navegate_itens_menu = menu.find('.li-links')
-const h1_menu = menu.find('.li-profile-menu .h1-emphasis')
+const navegateItensMenu = menu.find('.li-links')
+const h1Menu = menu.find('.li-profile-menu .h1-emphasis')
 
-const span_item_menu = '<span class="span-item minimize-menu-open"><span class="fa-solid fa-chevron-right"></span></span>'
+const buttonMenuContact = menu.find('.social-media-itens')
+const buttonExpandMenu = menu.find('.icon-expand')
 
-const button_menu_contact = $('body .article-menu .menu-ul-itens .group-li-items .li-item.social-media-itens')
-const button_expand_menu = $('body .article-menu .menu-ul-itens .li-profile-menu .icon-expand')
-
-button_menu_contact.on('click', function(){
-    let menu_contact = $('body .article-menu .menu-ul-itens .group-li-contacts')
-    let button_status = $('body .article-menu .menu-ul-itens .group-li-items .social-media-itens')
-
-    !menu_contact.hasClass('group-li-contacts-open') ? (menu_contact.addClass('group-li-contacts-open'), (button_status.addClass('icon-socialmedia-open'), button_status.parents('.group-li-items').addClass('socialmedia-open')), button_status.attr('title', 'Close social media details')) 
-    : (menu_contact.removeClass('group-li-contacts-open'), (button_status.removeClass('icon-socialmedia-open'), button_status.parents('.group-li-items').removeClass('socialmedia-open')), button_status.attr('title', 'Open social media details'))
-})
-
-button_expand_menu.on('click', function(){
-    const button_expand_menu = $(this)
-
-    let menu = $('.article-menu')
-    let menu_items = $('.article-menu .nav-article-menu')
-    let menu_contact = $('body .article-menu .menu-ul-itens .group-li-contacts')
-
-    if (!menu.hasClass('article-menu-expanded')) {
-        menu.addClass('article-menu-expanded') 
-        menu_items.addClass('nav-article-menu-expanded')
-        menu_contact.addClass('group-li-contacts-expanded')
-        button_expand_menu.attr('title', 'Minimize menu')
-
-    } else {
-        menu.removeClass('article-menu-expanded')
-        menu_items.removeClass('nav-article-menu-expanded')
-        menu_contact.removeClass('group-li-contacts-expanded')
-        button_expand_menu.attr('title', 'Expand menu')
-
-    }
-
-    NameStyleMenu(h1_menu)
-})
-
-navegate_itens_menu.on('click', function (){
+navegateItensMenu.on('click', function() {
     const className = $(this).attr('class').split(' ')
-    const classFilter = className[1]
-    const divs = $('section .div-section')
+    const classTypeName = className[1]
 
-    if (divs.length) {
-        divs.each(function (index){
-            switch (classFilter){
-                case 'li1':
-                    if (index === 1) {
-                        return $('html, body').animate({
-                            scrollTop: divs.eq(index).offset().top 
-                        }, 700)
-                    }
-                    break
-                case 'li2':
-                    if (index === 2) {
-                        return $('html, body').animate({
-                            scrollTop: divs.eq(index).offset().top + 130
-                        }, 700)
-                    }
-                    break
-                case 'li3':
-                    if (index === 3) {
-                        return $('html, body').animate({
-                            scrollTop: divs.eq(index).offset().top + 130
-                        }, 700)
-                    }
-                    break
-                case 'li4':
-                    if (index === 4) {
-                        return $('html, body').animate({
-                            scrollTop: divs.eq(index).offset().top + 130
-                        }, 700)
-                    }
-                    break
-            }
-        })
-    }
+    shiftNavegateItensMenu(classTypeName)
 })
 
-// -- menu mobile settings --
+buttonMenuContact.on('click', function(){
+    shiftMenuContact()
+})
 
-const screen_width = () => {
+buttonExpandMenu.on('click', function(){
+    shiftMenu($(this))
+    nameStyleMenu(h1Menu)
+})
 
-    let permission = false
-    screen_user_g.resize(function () { permission = true,  ResponsiveComponents($(this).width()) })
-
-    // If user don't move the window and current resolution under 650px
-    !permission ? ResponsiveComponents(screen_user_g.width()) : undefined
+function shiftMenu(event) { 
+    const menuItens = menu.find('.nav-article-menu')
+    const menuContact = menu.find('.group-li-contacts')
+ 
+    menu.toggleClass('article-menu-expanded')
+    menuItens.toggleClass('nav-article-menu-expanded')
+    menuContact.toggleClass('group-li-contacts-expanded')
+    $(event).attr('title', menu.hasClass('article-menu-expanded') ? 'Minimize menu' : 'Expand menu')
 }
 
-screen_width()
+function shiftMenuContact() {
+    const menuContact = menu.find('.group-li-contacts')
+    const iconButtonStatus = menu.find('.social-media-itens')
 
-// -- SECTION FUNCTIONS -- 
-const button_up = $('section .button-up')
+    menuContact.toggleClass('group-li-contacts-open')
+    iconButtonStatus.toggleClass('icon-socialmedia-open')
+    iconButtonStatus.parents('.group-li-items').toggleClass('socialmedia-open')
+    iconButtonStatus.attr('title', menuContact.hasClass('group-li-contacts-open') ? 'Close social media details' : 'Open social media details')
+}
 
-button_up.on('click', function (){
+function shiftNavegateItensMenu(classTypeName) {
     const divs = $('section .div-section')
+    let topValuePlus = 0
+    classTypeName === 'li1' ? topValuePlus = 50 : topValuePlus = 160
 
-    if (divs.length){
-        const divScroll = divs.eq(0)
+    //regex is based on "classTypeName (class - css)" from menu 'li' and its numbers
+    const regex = /li(\d+)/
+    const matchNumber = classTypeName.match(regex)
 
+    if (matchNumber && matchNumber[1]) {
+        const targetIndex = parseInt(matchNumber[1], 10)
+
+        if (divs.length) {
+            divs.each(function (index) {
+                if (index === targetIndex) {
+                     $('html, body').animate({
+                        scrollTop: divs.eq(index).offset().top + topValuePlus
+                    }, 700)
+    
+                    return false
+                }
+            })
+        }
+    }
+}
+
+// -- MAIN FUNCTIONS --
+const main = $('main')
+const footer = $('footer')
+
+const buttonUpMain = main.find('.button-up')
+buttonUpMain.on('click', function (){
+    const contentHTMLtarget = main.find('.div-section-titles')
+    shiftNavegateMainContents(contentHTMLtarget)
+})
+
+// email functions
+const buttonEmailFooter = footer.find('.email-footer')
+const buttonEmailSection = main.find('.email-section')
+const contentHTMLtarget = main.find('.div-section-content-t4')
+
+buttonEmailFooter.on('click', function (){
+    shiftNavegateMainContents(contentHTMLtarget)
+})
+
+buttonEmailSection.on('click', function (){
+    shiftNavegateMainContents(contentHTMLtarget)
+})
+
+function shiftNavegateMainContents(contentHTML) {
+    if (contentHTML.length){
+        let divScroll = contentHTML
         $('html, body').animate({
             scrollTop: divScroll.offset().top
         }, 700)
     }
+}
+
+const buttonSendEmail = main.find('.a-contact-send-email')
+buttonSendEmail.on('click', function (){
+    sendEmail()
 })
 
-// -- EMAIL FUNCTIONS --
-const navegate_email = $('body .email-section, .email-footer')
-const formEmail = $('section .form-email')
-const contentEmail = $('section .form-email .div-contact-content-email')
-const sendEmail = $('section .div-section-content-t4 .a-contact-send-email')
-
-navegate_email.on('click', function (){
-    const divs = $('section .div-section')
-
-    if (divs.length){
-        const divScroll = divs.eq(4)
-
-        $('html, body').animate({
-            scrollTop: divScroll.offset().top
-        }, 700)
-    }
-})
-
-sendEmail.on('click', function (){
+function sendEmail () {
     try {
+        const formEmail = main.find('.form-email')
+        const contentEmail = main.find('.div-contact-content-email')
+
         let name = contentEmail.find('input[name="name"]').val()
         let email = contentEmail.find('input[name="email"]').val()
         let message = contentEmail.find('textarea[name="message"]').val()
 
-        let messageAtention = $('<div class="message-email-inputs"><span>Oops! Check if all the data has been filled out, try again. </span></div>')
-        let messageAtentionEmail = $('<div class="message-email-validation"><span>E-mail provided is invalid. Check the typed credentials and try again!</span></div>')
+        let messageAtention = $('<div class="message-email-inputs"><span>Oops! Check if all the data has been filled out, try again. </span> <span class="fa-regular fa-circle-xmark close-message"></span></div>')
+        let messageAtentionEmail = $('<div class="message-email-validation"><span>E-mail provided is invalid. Check the typed credentials and try again!</span></span> <span class="fa-regular fa-circle-xmark close-message"></span></div>')
 
         let parentMessage = $('body section .div-section-content-t4 .div-contact-content .div-user-data-textarea')
 
@@ -165,26 +146,13 @@ sendEmail.on('click', function (){
     } catch (e) {
         console.log(`Error to send e-mail: ${e}`)
     }
-})
+}
 
-$('body').on('click', function () {
-    $('section .div-section-content-t4 .p-user-data').find('label, input').hasClass('type-active') ? 
-    ($('section .div-section-content-t4 .p-user-data').find('label, input').removeClass('type-active'), $('section .div-section-content-t4 .p-user-data').find('input').attr('placeholder', ''))
-     : undefined
-
-    $('.p-user-data input').each(function () { ResponsiveComponents(undefined, $(this)) })
-})
-
-// -- FOOTER FUNCTIONS --
-const footer_year = $('footer .div-footer-info .message .footer-year')
-const data_now = new Date()
-footer_year.text(data_now.getFullYear())
-
-// -- SCROLL FUNCTIONS --
-const screen_scroll = $(window)
+// scroll functions NOVAS APLICAÇÕES AQUI (REFATORAR CÓDIGO)
+const screenScroll = $(window)
 const section = $('section')
 
-screen_scroll.on('scroll', function (){
+screenScroll.on('scroll', function (){
     const scrollTop = $(this).scrollTop()
 
     if (section.length){     
@@ -223,13 +191,18 @@ screen_scroll.on('scroll', function (){
     }
 })
 
-// -- THEMER FUNCTIONS --
-const switch_theme = $('section .div-section-titles .buttons-config .switch-theme')
+// -- FOOTER FUNCTIONS --
+const footerYear = $('footer .div-footer-info .message .footer-year')
+const dataNow = new Date()
+footerYear.text(dataNow.getFullYear())
 
-switch_theme.on('click', function (){
+// -- THEMER FUNCTIONS --
+const switchTheme = $('section .div-section-titles .buttons-config .switch-theme')
+
+switchTheme.on('click', function (){
     localStorage.setItem('theme', '')
     $(this).toggleClass('dark light')
-    switch_theme.find('.icon-current-theme').toggleClass('fa-moon fa-sun')
+    switchTheme.find('.icon-current-theme').toggleClass('fa-moon fa-sun')
 
     if ($(this).hasClass('light')) { localStorage.theme = 'themelight', $('section .div-section-titles .buttons-config .switch-theme').attr('title', 'Change theme to dark')} 
     else { localStorage.theme = 'themedark', $('section .div-section-titles .buttons-config .switch-theme').attr('title', 'Change theme to light')
@@ -259,12 +232,12 @@ function Theme() {
 $(document).ready(function(){
     try {
         if(localStorage.theme === 'themelight') {
-            switch_theme.addClass('light')
-            switch_theme.find('.icon-current-theme').addClass('fa-sun')
+            switchTheme.addClass('light')
+            switchTheme.find('.icon-current-theme').addClass('fa-sun')
             $('section .div-section-titles .buttons-config .switch-theme').attr('title', 'Change theme to dark')
         } else {
-            switch_theme.addClass('dark')
-            switch_theme.find('.icon-current-theme').addClass('fa-moon')
+            switchTheme.addClass('dark')
+            switchTheme.find('.icon-current-theme').addClass('fa-moon')
             $('section .div-section-titles .buttons-config .switch-theme').attr('title', 'Change theme to light')
         }
     } catch(e) {
@@ -274,36 +247,49 @@ $(document).ready(function(){
     Theme()
 })
 
-function ResponsiveComponents(window_size, component) {
+function ResponsiveComponents(windowSize, component) {
 
     //Type Screen
-    if (window_size !== undefined && window_size !== null) {
-        window_size <= 650 ? NameStyleMenu(h1_menu, true) 
-        : NameStyleMenu(h1_menu, false), menu.hasClass('article-menu-expanded') ? menu.removeClass('article-menu-expanded') : undefined, menu.find('.nav-article-menu').hasClass('nav-article-menu-expanded') ? menu.find('.nav-article-menu').removeClass('nav-article-menu-expanded') : undefined
+    if (windowSize !== undefined && windowSize !== null) {
+        windowSize <= 650 ? nameStyleMenu(h1Menu, true) 
+        : nameStyleMenu(h1Menu, false), menu.hasClass('article-menu-expanded') ? menu.removeClass('article-menu-expanded') : undefined, menu.find('.nav-article-menu').hasClass('nav-article-menu-expanded') ? menu.find('.nav-article-menu').removeClass('nav-article-menu-expanded') : undefined
     }
 
     //Type Input
     if (component !== undefined) { component.val() !== '' ? (component.parent().find('label').addClass('type-active'), component.addClass('type-active')) : undefined }
 }
 
-function NameStyleMenu(element, custom_setting = undefined) {
-    const style_h1 = $(element)
-    const style_h1_span = style_h1.find('.span-title-emphasis')
+function nameStyleMenu(element, customSetting = undefined) {
+    const styleH1 = $(element)
+    const styleH1Span = styleH1.find('.span-title-emphasis')
 
     if (menu.hasClass('article-menu-expanded')) {
-        style_h1.find('.span-item1-h1-style').length < 1 ? (style_h1_span.append('<span class="span-item1-h1-style">ucas .O</span>'), style_h1.append('<span class="span-item2-h1-style">Web Developer</span>')) : undefined
+        styleH1.find('.span-item1-h1-style').length < 1 ? (styleH1Span.append('<span class="span-item1-h1-style">ucas .O</span>'), styleH1.append('<span class="span-item2-h1-style">Web Developer</span>')) : undefined
     } else {
-        style_h1.find('.span-item1-h1-style').length === 1 ? style_h1.find('.span-item1-h1-style, .span-item2-h1-style').remove() : undefined
+        styleH1.find('.span-item1-h1-style').length === 1 ? styleH1.find('.span-item1-h1-style, .span-item2-h1-style').remove() : undefined
     }
 
     // -- responsive settings
     if (!menu.hasClass('article-menu-expanded')) {
-        if (custom_setting === true) {
-            style_h1.find('.span-item1-h1-style').length < 1 ? (style_h1_span.append('<span class="span-item1-h1-style">ucas .O</span>'), style_h1.append('<span class="span-item2-h1-style">Web Developer</span>')) : undefined
+        if (customSetting === true) {
+            styleH1.find('.span-item1-h1-style').length < 1 ? (styleH1Span.append('<span class="span-item1-h1-style">ucas .O</span>'), styleH1.append('<span class="span-item2-h1-style">Web Developer</span>')) : undefined
         } 
         
-        if (custom_setting === false) {
-            style_h1.find('.span-item1-h1-style').length === 1 ? style_h1.find('.span-item1-h1-style, .span-item2-h1-style').remove() : undefined
+        if (customSetting === false) {
+            styleH1.find('.span-item1-h1-style').length === 1 ? styleH1.find('.span-item1-h1-style, .span-item2-h1-style').remove() : undefined
         }
     }
 }
+
+// -- menu mobile settings --
+
+const screen_width = () => {
+
+    let permission = false
+    screenUserG.resize(function () { permission = true,  ResponsiveComponents($(this).width()) })
+
+    // If user don't move the window and current resolution under 650px
+    !permission ? ResponsiveComponents(screenUserG.width()) : undefined
+}
+
+screen_width()
