f4010e0cae388c5c5a63cdb0502024395e8a73c3
Lucas <153150990+LukasO20@users.noreply.github.com>
2024-11-30 22:51:09 -0300

    Added dark logo and Automatic scroll between 'sections'
    - Added new logo (dark mode)
    - New function ('shiftScrollView', 'scrollToElement') to shift scroll between sections
    - Improvements of dartheme.scss and layout.scss

--- redesign/libs/js/layout.js
+++ redesign/libs/js/layout.js
@@ -1,8 +1,42 @@
+// variables
+const container = document.querySelector('.container')
+
 // scroll functions
+const toScrollTop = document.querySelector('.scroll.top')
+const toScrollBottom = document.querySelector('.scroll.bottom')
+
 window.addEventListener('scroll', () => {
-    console.log('SCROLLED!!')
+    shiftScrollView()
 })
 
-function automaticScroll (scrollData) {
+const scrollToElement = (el) => {
+    el.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
 
+    //spacing adjustments
+    const styles = window.getComputedStyle(el)
+    const marginTop = parseInt(styles.marginTop)
+    const paddingTop = parseInt(styles.paddingTop)
+
+    if (marginTop + paddingTop > 0) {
+        window.scrollBy(0, -(marginTop + paddingTop))
+    }
+}
+
+let lastTopScroll = 0
+const shiftScrollView = () => {
+    const currentScroll = document.documentElement.scrollTop
+    if (currentScroll > lastTopScroll) {     
+        if (toScrollBottom !== undefined) {
+            scrollToElement(toScrollBottom)
+        }
+    } else if (currentScroll < lastTopScroll) {
+        if (toScrollTop !== undefined) {
+            scrollToElement(toScrollTop)
+        }
+    } 
+    console.log('VALUES - ', ' CURRENT - ', currentScroll, ' LAST - ', lastTopScroll)
+    lastTopScroll = currentScroll <= 0 ? 0 : currentScroll
 }
