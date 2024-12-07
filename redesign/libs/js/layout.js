c577a9189399abba6e3e740fa01b1dd9f66e7212
Lucas <153150990+LukasO20@users.noreply.github.com>
2024-12-07 00:14:14 -0300

    Added scroll snapping during scroll page - 'layout.js'
    - Improvements on scroll indicator (add 'shiftStyleIndicator ')
    - Improvements on layout.js ('shiftScrollView ')

--- redesign/libs/js/layout.js
+++ redesign/libs/js/layout.js
@@ -4,10 +4,25 @@ const container = document.querySelector('.container')
 // scroll functions
 const toScrollTop = document.querySelector('.scroll.top')
 const toScrollBottom = document.querySelector('.scroll.bottom')
+const scrollIndicator = document.querySelectorAll('.container-scroll-indicator label')
 
 window.addEventListener('scroll', () => {
     shiftScrollView()
 })
+
+const shiftStyleIndicator = (scrollDataBefore, scrollDataCurrent, element) => {
+    if (element !== undefined) {
+        element.forEach((item, i) => {
+            item.classList.remove('on')
+
+            if (scrollDataCurrent > scrollDataBefore) {     
+                if (i === 1) { item.classList.add('on') }
+            } else if (scrollDataCurrent < scrollDataBefore) {
+                if (i === 0) { item.classList.add('on') }
+            } 
+        })
+    }
+}
 
 const scrollToElement = (el) => {
     el.scrollIntoView({
@@ -15,6 +30,7 @@ const scrollToElement = (el) => {
     })
 }
 
+let lastTopScroll = 0
 const shiftScrollView = () => {
     const currentScroll = document.documentElement.scrollTop
     if (currentScroll > lastTopScroll) {     
@@ -26,8 +42,6 @@ const shiftScrollView = () => {
             scrollToElement(toScrollTop)
         }
     } 
-    console.log('VALUES - ', ' CURRENT - ', currentScroll, ' LAST - ', lastTopScroll)
+    shiftStyleIndicator(lastTopScroll, currentScroll, scrollIndicator)
     lastTopScroll = currentScroll <= 0 ? 0 : currentScroll
 }
-
-let lastTopScroll = 0
